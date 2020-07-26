d15c5734877a9096bc84f63f09a7b1b9ccb9c4d5
Hiroyuki Sato <hiroysato@gmail.com>
2020-07-26 17:15:17 +0900

    Update test

--- test/macos/link_speed.cc
+++ test/macos/link_speed.cc
@@ -7,9 +7,21 @@ using std::string;
 TEST(LinkSpeed, MTU) {
     uint32_t speed;
     uint32_t mtu;
+    ucs_status_t status;
 
-    link_speed("en0",&speed,&mtu);
+    status = link_speed("en0",&speed,&mtu);
 
+    EXPECT_EQ(UCS_OK, status);
     EXPECT_EQ(1500, mtu);
     EXPECT_EQ(304200000, speed); // TODO get interface value
+}
+
+TEST(LinkSpeed, MTUError) {
+    uint32_t speed;
+    uint32_t mtu;
+    ucs_status_t status;
+
+    status = link_speed("en10",&speed,&mtu);
+
+    EXPECT_EQ(UCS_ERR_IO_ERROR, status);
 }
